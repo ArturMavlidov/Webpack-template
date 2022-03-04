e2b549de368bff40be8a94a47121c468122d6345
ArturMavlidov <artur.mavlidov75@mail.ru>
2022-03-05 01:42:00 +0300

    Init project

---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+module.exports = {
+  mode: "development",
+  entry: "./src/index.js",
+  output: {
+    filename: "[name].js",
+    path: path.resolve(__dirname, "build"),
+    clean: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.s[ac]ss$/i,
+        use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
+      },
+       {
+        test: /\.html$/i,
+        loader: "html-loader",
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.join(__dirname, "public/index.html"),
+    }),
+    new MiniCssExtractPlugin(),
+  ],
+  devServer: {
+    static: {
+      directory: path.join(__dirname, "public"),
+    },
+    compress: true,
+    port: 9000,
+  },
+};
